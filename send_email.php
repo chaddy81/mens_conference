68742055199cd67acb70165779fec9786a8a3535
Chad Bartels <chad.bartels@gmail.com>
2015-11-30 09:23:02 -0500

    Testing email sending functionality

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+
+$sendgrid_username = $_ENV['SENDGRID_USERNAME'];
+$sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
+$to                = $_ENV['SEND_MAIL_TO'];
+
+$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
+$email    = new SendGrid\Email();
+$email->addTo($to)->
+       setFrom($to)->
+       setSubject('test email')->
+       setText('Owl are you doing?')->
+       setHtml('<strong>%how% are you doing?</strong>')->
+       addHeader('X-Sent-Using', 'SendGrid-API')->
+       addHeader('X-Transport', 'web');
+
+$response = $sendgrid->send($email);
+var_dump($response);
+?>
