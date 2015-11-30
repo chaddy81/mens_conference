15049d5a2f48fc67730e75575593f9997ae5c066
Chad Bartels <chad.bartels@gmail.com>
2015-11-30 09:57:13 -0500

    Updating contact script

--- send_email.php
+++ send_email.php
@@ -5,15 +5,21 @@ $sendgrid_username = $_ENV['SENDGRID_USERNAME'];
 $sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
 $to                = $_ENV['SEND_MAIL_TO'];
 
-$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
-$email    = new SendGrid\Email();
-$email->addTo($to)->
-       setFrom($to)->
-       setSubject('test email')->
-       setText('Owl are you doing?')->
-       setHtml('<strong>%how% are you doing?</strong>')->
-       addHeader('X-Sent-Using', 'SendGrid-API')->
-       addHeader('X-Transport', 'web');
+if (!empty($_POST)) {
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
 
-$sendgrid->send($email);
+  $sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
+  $email    = new SendGrid\Email();
+  $email->addTo($to)->
+         setFrom($to)->
+         setSubject('Contact From Mens Conference Site')->
+         setText($email . '/r/n' . $phone . '/r/n' . $message)->
+         setHtml($email . '<br />' . $phone . '<br />' . $message)->
+         addHeader('X-Sent-Using', 'SendGrid-API')->
+         addHeader('X-Transport', 'web');
+
+  $sendgrid->send($email);
+}
 ?>
